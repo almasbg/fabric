1be3a3edd6e4e3b2bac2023b3f06dfd43ce569ae
almasbg <42900111+almasbg@users.noreply.github.com>
2018-09-30 22:19:37 -0500

    Create fab_ipmon_dev.py

---
+++ fab_ipmon_dev.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from getpass import getpass
+from fabric.api import *
+import sys
+import os
+#@env.gateway = 'username@IPofgatewayHost'
+
+
+env.password = getpass('Enter the password for: ')
+env.sudo_password = env.password
+list = []
+
+print(file)
+# file function needs to be added before execution any other tasks for multiple host list in file hosts
+# how to use this script: fab -f fab_ipmon_dev.py file:name=hosts ipmon_status
+def file(**kwargs):
+        for key, value in kwargs.iteritems():
+                hosts = value
+        with open(hosts, "r") as file:
+                for line in file:
+                        list.append(line)
+                env.hosts = list;
+
+def ipmon_restart():
+        sudo("/etc/init.d/IPmonX restart")
+
+def ipmon_status():
+        sudo('sudo /etc/init.d/IPmon status')
+
+def ipmon_disk():
+        #env.user = 'user'
+        #env.password = 'password'      
+        sudo("find /apps/IPsoft/IPmon/var -type f -not -name '*.gz' -name '*log*' -name '*201*' -exec gzip -9v {} \;")
+        sudo("find /apps/IPsoft/IPmon/var -type f -not -name '*.gz' -name '*service-perfdata*201*' -exec gzip -9v {} \;")
+        sudo("find /apps/IPsoft/IPmon/engine/jmx/log/jmx1.2/ -type f -not -name '*.gz' -name 'engine.log.201*' -exec gzip -9v {} \;")
